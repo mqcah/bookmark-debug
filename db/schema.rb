6f24bdd6601ec19fc9f2eb3d90dd2ad59f2efb90
vagrant <vagrant@localhost.localdomain>
2020-09-07 03:29:37 +0000

    book#index

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_06_21_071304) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
